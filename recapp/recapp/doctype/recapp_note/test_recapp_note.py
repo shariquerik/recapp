f3692806ce704d82e1a59da6a105d3d0b73966fb
Shariq Ansari <sharique.rik@gmail.com>
2023-02-11 16:08:26 +0530

    feat: added recapp note doctype to store daily notes

---
+++ recapp/recapp/doctype/recapp_note/test_recapp_note.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Shariq Ansari and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRecappNote(FrappeTestCase):
+	pass
